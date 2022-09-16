2662ba4ba9fa94a97ca0908c6f903b9e902842ef
s8189882 <s8189882@gmail.com>
2022-09-16 23:04:46 +0530

    create main branch

---
+++ Mood Analyzer/src/com/demo/moodanalyser/MoodAnalyzer.java
@@ -0,0 +1,8 @@
+package com.demo.moodanalyser;
+
+public class MoodAnalyzer {	
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Mood Analyser development using JUnit Testing!");
+	}
+}
